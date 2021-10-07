677e69e4b7d2dae5bd5d1436881dd67a4db177a3
Selwob <66890383+Selwob@users.noreply.github.com>
2021-10-07 14:24:36 +0900

    some stuff

---
+++ paginator.js
@@ -0,0 +1,75 @@
+class Paginator {
+    
+    currentPage = 1;
+    numberPerPage = 5;
+    numberOfPages = 1;
+    
+
+    constructor (numberOfObjects, numberPerPage, onPageChange){
+        this.numberOfObjects = numberOfObjects;
+        this.numberOfPages = Math.ceil(numberOfObjects / numberPerPage);
+        this.numberPerPage = numberPerPage;
+        this.onPageChange = onPageChange;
+    }
+
+    getOffset() {
+        let firstItemIndex = this.numberPerPage * (this.currentPage - 1);
+        return firstItemIndex;
+    }
+
+    nextPage() {
+        this.currentPage += 1;
+        let offset = this.getOffset();
+        this.onPageChange(offset, offset + this.numberOfPages);
+        this.check();
+    }
+
+    previousPage() {
+        this.currentPage -= 1;
+        let offset = this.getOffset();
+        this.onPageChange(offset, offset + this.numberOfPages);
+        this.check();
+    }
+    
+    firstPage() {
+        this.currentPage = 1;
+        let offset = this.getOffset();
+        this.onPageChange(offset, offset + this.numberOfPages);
+        this.check();
+    }
+
+    lastPage() {
+        this.currentPage = this.numberOfPages;
+        let offset = this.getOffset();
+        this.onPageChange(offset, offset + this.numberOfPages);
+        this.check();
+    }
+     
+    check() {
+        document.getElementById("first").disabled = this.currentPage == 1 ? true : false;
+        document.getElementById("previous").disabled = this.currentPage == 1 ? true : false;
+        document.getElementById("next").disabled = this.currentPage == this.numberOfPages ? true : false;
+        document.getElementById("last").disabled = this.currentPage == this.numberOfPages ? true : false;
+    }
+
+    render() {
+        let el = document.createElement("div");
+        el.innerHTML = `<div id="outer">
+        <div id="inner"; style="position: relative; left: 300px;">
+            <input type="button" class="navButton" id="first" value="first" />
+            <input type="button" class="navButton" id="previous" value="previous" />
+            <input type="button" class="navButton" id="next" value="next" />
+            <input type="button" class="navButton" id="last" value="last" />
+        </div>
+    </div>`
+        el.querySelector('#first').onclick = () => this.firstPage();
+        el.querySelector('#previous').onclick = () => this.previousPage();
+        el.querySelector('#next').onclick = () => this.nextPage();
+        el.querySelector('#last').onclick = () => this.lastPage();
+             
+
+        return el;
+        
+    }
+
+}
--- todo.md
+++ todo.md
@@ -1,24 +1,33 @@
 TODO:
+-1.
+fix paginator first/last disable function
+fix edit
+make the page part of the location hash (show correct article list if given url)
 
 0.
-Fix stuff
+Fix stuff -> added code to end of render resetting window.location.hash
+-> fixed edit and back buttons
+-> fixed edit/save + tags
+-> more fixes to tags (endless adding of "#")
 
 1. 
 Create a class for database that has get-page, get-article, update-article, remove-article, count methods.
 
 Google - using browser local storage API
 
-2.
-Pagination
-
-3. 
-Change navigation to use browser navigation API
-
 4.
 Sign Up / Login Page  --> Store in browser local storage
 
 5.
 Like button, comments
+
+6.
+Write unit test
+
+7.
+SOLID principles
+
+
 
 
 
